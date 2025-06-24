165718ae3c5e069b63152a87886b4064db046ae6
okorie2 <ellahokorie@gmail.com>
2025-06-24 09:35:42 +0100

    feat:DX-2570; add anchor plugin components and utilities for draftjs link plugin

---
+++ src/app/modules/common/RichEditor/anchor/utils/URLUtils.ts
@@ -0,0 +1,26 @@
+import prependHttp from "prepend-http";
+import urlRegex from "./urlRegex";
+import mailRegex from "./mailRegex";
+
+const URLUtils = {
+  isUrl(text: string): boolean {
+    return urlRegex().test(text);
+  },
+
+  isMail(text: string): boolean {
+    return mailRegex().test(text);
+  },
+
+  normaliseMail(email: string): string {
+    if (email.toLowerCase().startsWith("mailto:")) {
+      return email;
+    }
+    return `mailto:${email}`;
+  },
+
+  normalizeUrl(url: string): string {
+    return prependHttp(url);
+  },
+};
+
+export default URLUtils;
