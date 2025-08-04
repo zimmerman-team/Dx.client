136829f49097840c6439e2864c15fb9c776321bf
okorie2 <ellahokorie@gmail.com>
2025-08-04 15:57:50 +0100

    fix: update SVG attributes to camelCase for consistency

--- src/app/assets/icons/ChevronUp.tsx
+++ src/app/assets/icons/ChevronUp.tsx
@@ -13,9 +13,9 @@ const ChevronUp: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
       <path
         d="M6 9.5L12 15.5L18 9.5"
         stroke="black"
-        stroke-width="1.6"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
