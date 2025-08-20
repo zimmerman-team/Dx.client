8c33a6d19e2dcd596e12b833de2cdf3c1594ccd3
okorie2 <ellahokorie@gmail.com>
2025-08-21 00:50:04 +0100

    chore: remove logs

--- src/app/utils/draftjs/getStyleEl.ts
+++ src/app/utils/draftjs/getStyleEl.ts
@@ -36,31 +36,21 @@ export function updateDynamicStylesOnRender(editorState: EditorState) {
 
     // 🔹 Build CSS from block data
     let blockStyles = "";
-    console.log(data, "data in getStyleEl");
     if (data.has("color")) {
       blockStyles += `color: ${data.get("color")};`;
       const className = `COLOR-${data.get("color").replace("#", "")}`;
       cssRules += `li.${className}::marker { color: ${data.get("color")}; }\n`;
     }
-    // if (data.has("bgColor")) {
-    //   blockStyles += `background-color: ${data.get("bgColor")};`;
-    // }
-    // if (data.has("bold")) {
-    //   blockStyles += `font-weight: bold;`;
-    // }
-    // if (data.has("italics")) {
-    //   blockStyles += `font-style: italic;`;
-    // }
-    // if (data.has("fontStyle")) {
-    //   blockStyles += `font-style: ${data.get("fontStyle")};`;
-    // }
-    // if (data.has("fontFamily")) {
-    //   blockStyles += `font-family: ${data.get("fontFamily")};`;
-    // }
-
-    // if (blockStyles) {
-    //   cssRules += `.BLOCK-${blockKey} { ${blockStyles} }\n`;
-    // }
+    if (data.has("fontFamily")) {
+      blockStyles += `font-family: ${data.get("fontFamily")};`;
+      const className = `FONT_FAMILY_${data
+        .get("fontFamily")
+        .replace(/\s/g, "_")
+        .toUpperCase()}`;
+      cssRules += `li.${className}::marker { font-family: ${data.get(
+        "fontFamily"
+      )}; }\n`;
+    }
   });
   styleEl.textContent = (styleEl.textContent || "") + cssRules;
 }
